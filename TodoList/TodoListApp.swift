c7abc2cedd9db1a42ca19992f8ea556787f1bc26
Suneel1506 <nsuneelkumar98@gmail.com>
2024-11-19 10:50:07 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by asset 212 on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
